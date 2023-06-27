8de544d347c4f2e221befc2c1525f9859032757d
Asharudeen Ali KT <131525899+achu-BSL@users.noreply.github.com>
2023-06-27 19:23:08 +0530

    Update login.mjs

--- assets/js/admin/login.mjs
+++ assets/js/admin/login.mjs
@@ -6,39 +6,6 @@ const emailInp = document.getElementById('email')
 const passwordInp = document.getElementById('password')
 
 
-console.log(form)
-console.log("HIHIIHIHIIHI")
-
-// const verifyEmailBtn = document.getElementById('verify-email')
-// const sendOTPBtn = document.getElementById('send-otp')
-// const verifyOTPBtn = document.getElementById('verify-otp')
-// const otpInp = document.getElementById('otp')
-// let OTPSended = false
-
-
-
-// emailInp.addEventListener("keyup", ()=>{
-//     OTPSended = false
-//     sendOTPBtn.disabled = true
-//     verifyOTPBtn.disabled = true
-// })
-
-
-// verifyEmailBtn.addEventListener('click', async()=>{
-//     const response = await fetchData('/api/admin/login', 
-//     'POST', 
-//     {
-//         admin_email : emailInp.value.trim()
-//     })
-
-//     const data = await response.json()
-//     if(data?.msg !== "email not found"){
-//         setSuccess(emailInp)
-//         sendOTPBtn.disabled = false
-//         verifyOTPBtn.disabled = false
-//     }
-// })
-
 
 form.addEventListener("submit", async (e)=>{
     e.preventDefault()
@@ -67,25 +34,3 @@ form.addEventListener("submit", async (e)=>{
         }
     }
 })
-
-
-//SendOTP
-// sendOTPBtn.addEventListener('click', async ()=>{
-//     const response = await fetchData('/api/generate-otp', 'GET')
-//     if(response.ok){
-//         OTPSended = true
-//         const data = await response.text()
-//         console.log(JSON.parse(data).OTP)
-//     }
-// })
-
-
-// verifyOTPBtn.addEventListener('click', async()=>{
-//     const response = await fetchData('/admin/login', 'POST', {admin_email: emailInp.value})
-//     if(response.ok && OTPSended) {
-//         // const response = await fetchData('/api/verify-otp', 'POST', {OTP: otpInp.value})
-//         setSuccess(otpInp)
-//         window.location.href = '/api/'
-//     }
-//     else setError(otpInp, 'Not match')
-// })
